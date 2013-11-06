f0bcada9fb5638ced0895882a41bdb205e1bb66a
kybernetikos <kybernetikos@gmail.com>
2013-11-06 12:16:08 +0000

    initial checkin under the new name

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+    var browsers = [{
+        browserName: "firefox",
+        version: "19",
+        platform: "XP"
+    }, {
+        browserName: "chrome",
+        platform: "XP"
+    }, {
+        browserName: "chrome",
+        platform: "linux"
+    }, {
+        browserName: "internet explorer",
+        platform: "WIN8",
+        version: "10"
+    }, {
+        browserName: "internet explorer",
+        platform: "VISTA",
+        version: "9"
+    }];
+
+    grunt.initConfig({
+        connect: {
+            server: {
+                options: {
+                    base: "",
+                    hostname: "",
+                    port: 9999
+                }
+            }
+        },
+        'saucelabs-jasmine': {
+            all: {
+                options: {
+                    urls: ["http://127.0.0.1:9999/spec/index.html"],
+                    tunnelTimeout: 5,
+                    build: process.env.TRAVIS_JOB_ID,
+                    concurrency: 3,
+                    browsers: browsers,
+                    testname: "jasmine tests"
+                }
+            }
+        },
+        watch: {}
+    });
+
+    // Loading dependencies
+    for (var key in grunt.file.readJSON("package.json").devDependencies) {
+        if (key !== "grunt" && key.indexOf("grunt") === 0) grunt.loadNpmTasks(key);
+    }
+
+    grunt.registerTask("serve", ["connect", "watch"]);
+    grunt.registerTask("saucelabs-test", ["connect", "saucelabs-jasmine"]);
+};
